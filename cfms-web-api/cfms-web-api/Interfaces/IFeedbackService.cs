51a04cdb9b9d461a7077a204463cb0657e9f6777
Emandleni <emandlenimoyo@gmail.com>
2024-05-03 08:58:47 +0200

    Defined IFeedbackService interface.

---
+++ cfms-web-api/cfms-web-api/Interfaces/IFeedbackService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using cfms_web_api.Models;
+
+namespace cfms_web_api.Interfaces
+{
+	public interface IFeedbackService
+	{
+        List<Feedback> GetAllFeedback();
+        Feedback GetFeedbackById(int id);
+        List<Feedback> AddFeedback(Feedback Feedback);
+        List<Feedback> UpdateFeedback(int id, Feedback Feedback);
+        List<Feedback> DeleteFeedback(int id);
+    }
+}
+
