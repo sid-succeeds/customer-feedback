472cfb99ea8186d8f05c611d50233c8325c8a0e3
Emandleni <emandlenimoyo@gmail.com>
2024-05-03 09:06:53 +0200

    Created FeedbackService controller to provide a layer of abstraction over the data access methods defined in the IFeedbackRepository, encapsulating the logic for handling feedback operations.

---
+++ cfms-web-api/cfms-web-api/Controller/FeedbackService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using cfms_web_api.Interfaces;
+using cfms_web_api.Models;
+
+namespace cfms_web_api.Controller
+{
+    public class FeedbackService : IFeedbackService
+    {
+        private readonly IFeedbackRepository _FeedbackRepository;
+
+        public FeedbackService(IFeedbackRepository FeedbackRepository)
+        {
+            _FeedbackRepository = FeedbackRepository;
+        }
+
+        public List<Feedback> GetAllFeedback()
+        {
+            return _FeedbackRepository.GetAllFeedback();
+        }
+
+        public Feedback GetFeedbackById(int id)
+        {
+            return _FeedbackRepository.GetFeedbackById(id);
+        }
+
+        public List<Feedback> AddFeedback(Feedback Feedback)
+        {
+            return _FeedbackRepository.AddFeedback(Feedback);
+        }
+
+        public List<Feedback> UpdateFeedback(int id, Feedback Feedback)
+        {
+            return _FeedbackRepository.UpdateFeedback(Feedback);
+        }
+
+        public List<Feedback> DeleteFeedback(int id)
+        {
+            return _FeedbackRepository.DeleteFeedback(id);
+        }
+    }
+}
+
