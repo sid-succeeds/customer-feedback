ff5d94b30974a6e6221a6a4436078d397f999b2f
Emandleni <emandlenimoyo@gmail.com>
2024-05-03 08:52:57 +0200

    Defined the Feedback model. Removed scaffolding template class WeatherForecast and WeatherForecastController

---
+++ cfms-web-api/cfms-web-api/Models/Feedback.cs
@@ -0,0 +1,24 @@
+﻿namespace cfms_web_api.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime SubmittedDate { get; set; }
+
+        public Feedback(int id, string customerName, string email, string subject, string message, DateTime submittedDate)
+        {
+            Id = id;
+            CustomerName = customerName;
+            Email = email;
+            Subject = subject;
+            Message = message;
+            SubmittedDate = submittedDate;
+        }
+    }
+
+}
+
