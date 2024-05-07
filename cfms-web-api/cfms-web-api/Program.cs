2012f6500e1868ed3176056eaceac196d40b145e
Emandleni <emandlenimoyo@gmail.com>
2024-05-07 09:56:58 +0200

    Registered ICustomerService ICustomerRepository interfaces

--- cfms-web-api/cfms-web-api/Program.cs
+++ cfms-web-api/cfms-web-api/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<IFeedbackService, FeedbackService>();
 builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
