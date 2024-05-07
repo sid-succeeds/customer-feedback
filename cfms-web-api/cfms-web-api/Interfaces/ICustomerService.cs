7b6712ffe6f3d19abc09df7f02c22c6f3be93338
Emandleni <emandlenimoyo@gmail.com>
2024-05-07 09:15:32 +0200

    Define interfaces ICustomerRepository ICustomerService

---
+++ cfms-web-api/cfms-web-api/Interfaces/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using cfms_web_api.Models;
+
+namespace cfms_web_api.Interfaces
+{
+	public interface ICustomerService
+	{
+        List<Customer> GetAllCustomers();
+        Customer GetCustomerById(int id);
+        List<Customer> AddCustomer(Customer customer);
+        List<Customer> UpdateCustomer(int id, Customer customer);
+        List<Customer> DeleteCustomer(int id);
+    }
+}
+
